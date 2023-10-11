53289aa0d3f7b8e746389f08dc34c843701b0ccd
rodrigofilhoo <rodrigofilhogt@gmail.com>
2023-10-10 22:08:01 -0300

    Tratamento de erros ❌⚠️

--- src/main/java/br/com/cruzeiro/todolist/user/UserController.java
+++ src/main/java/br/com/cruzeiro/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.cruzeiro.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel UserModel){
+    @Autowired
+    private IUserRepository userRepository;
 
-        System.out.println(UserModel.getUsername());
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel UserModel) {
+        var user = this.userRepository.findByUsername(UserModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existente");
+        }
+        var userCreated = this.userRepository.save(UserModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
+
 }
