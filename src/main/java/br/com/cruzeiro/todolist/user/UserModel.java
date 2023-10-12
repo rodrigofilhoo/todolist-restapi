b9ce82eb0c65aab2ae986ef9d5140d1bfd3258e1
rodrigofilhoo <rodrigofilhogt@gmail.com>
2023-10-11 22:20:38 -0300

    Tabela banco de dados cirada 🙇‍♂️🙏

--- src/main/java/br/com/cruzeiro/todolist/user/UserModel.java
+++ src/main/java/br/com/cruzeiro/todolist/user/UserModel.java
@@ -2,14 +2,30 @@
 
 package br.com.cruzeiro.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users") //nome do bando de dados
 public class UserModel {
 
-    //Se não tiver modificador na frente = public
+    @Id
+    @GeneratedValue(generator = "UUID") //gera automaticante o UUID
+    private UUID id; //chave primária passada com uuid
+
+    @Column(unique = true) //apenas um usuário (apenas o que estiver em baixo do Column
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; // exibe a hora emm que o usuário foi criado
 
 }
